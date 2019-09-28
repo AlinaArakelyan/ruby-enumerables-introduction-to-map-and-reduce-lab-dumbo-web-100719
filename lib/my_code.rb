9c4117c2ba6af896b5169b1a8f56df45dbb294bf
Alina Arakelyan <alinaarakelyan90@gmail.com>
2019-09-28 18:17:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,9 +53,8 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
-    if source_array[i] == !source_array[i]
+   return false if source_array[i] == !source_array[i]
       i += 1
-    return false
   end
  return true
 end
